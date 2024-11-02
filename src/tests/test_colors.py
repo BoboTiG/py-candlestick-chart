41687cb10b5903cf1bfd70a455ce8e85e6d89b98
Mickaël Schoentgen <contact@tiger-222.fr>
2024-11-02 17:00:39 +0100

    feat(tests): improve `colors` module coverage

--- src/tests/test_colors.py
+++ src/tests/test_colors.py
@@ -1,19 +1,36 @@
 import pytest
 
-from candlestick_chart.colors import color
+from candlestick_chart import colors
 
 
 @pytest.mark.parametrize(
     ("custom_color", "expected"),
     [
         ("", "𓅂"),
-        ("red", "\x1b[91m𓅂\x1b[00m"),
-        ((255, 0, 0), "\x1b[38;2;255;0;0m𓅂\x1b[00m"),
+        ("green", "\x1b[92m𓅂\x1b[00m"),
+        ((0, 255, 0), "\x1b[38;2;0;255;0m𓅂\x1b[00m"),
         # ANSI color
-        ("91m", "\x1b[91m𓅂\x1b[00m"),
+        ("92m", "\x1b[92m𓅂\x1b[00m"),
         # ANSI color + background
-        ("91;47m", "\x1b[91;47m𓅂\x1b[00m"),
+        ("92;47m", "\x1b[92;47m𓅂\x1b[00m"),
     ],
 )
 def test_color(custom_color: str, expected: str) -> None:
-    assert color("𓅂", custom_color) == expected
+    assert colors.color("𓅂", custom_color) == expected
+
+
+@pytest.mark.parametrize(
+    ("color_name", "expected"),
+    [
+        ("blue", "\x1b[94mfoo\x1b[00m"),
+        ("bold", "\x1b[01mfoo\x1b[00m"),
+        ("cyan", "\x1b[96mfoo\x1b[00m"),
+        ("grey", "\x1b[90mfoo\x1b[00m"),
+        ("magenta", "\x1b[95mfoo\x1b[00m"),
+        ("red", "\x1b[91mfoo\x1b[00m"),
+        ("white", "\x1b[97mfoo\x1b[00m"),
+        ("yellow", "\x1b[93mfoo\x1b[00m"),
+    ],
+)
+def test_colors(color_name: str, expected: str) -> None:
+    assert getattr(colors, color_name)("foo") == expected
