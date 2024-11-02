6770dc8151d3d2b7a4a3baa71fd101b9d2fae800
Mickaël Schoentgen <contact@tiger-222.fr>
2024-11-02 14:02:07 +0100

    feat(tests): improve coverage

---
+++ src/tests/test_candle_set.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from candlestick_chart.candle_set import CandleSet
+from candlestick_chart.utils import parse_candles_from_csv
+
+EXAMPLES = Path(__file__).parent.parent.parent / "examples"
+
+
+def test_compute_all() -> None:
+    candles = parse_candles_from_csv(EXAMPLES / "BTC-USD.csv")
+    candle_set = CandleSet(candles)
+
+    assert candle_set.candles == candles
+    assert candle_set.min_price == 28_722.755859
+    assert candle_set.max_price == 67_673.742188
+    assert candle_set.min_volume == 0.0
+    assert candle_set.max_volume == 350_967_941_479.0
+    assert candle_set.variation == 133.037_198_615_531_42
+    assert candle_set.average == 46_375.191_043_259_58
+    assert candle_set.last_price == 67_566.828_125
+    assert candle_set.cumulative_volume == 15_472_149_666_480.0
---
+++ src/tests/test_chart.py
@@ -0,0 +1,115 @@
+import pytest
+from rich.console import Console
+
+from candlestick_chart import constants
+from candlestick_chart.candle import Candle
+from candlestick_chart.chart import Chart
+
+CANDLE = Candle(open=1, high=2, low=0.5, close=1.2, volume=1_234_567)
+WIDTH = 80
+HEIGHT = 24
+
+
+@pytest.fixture
+def chart() -> Chart:
+    return Chart([CANDLE], title="BTC/USDT", width=WIDTH, height=HEIGHT)
+
+
+def test_rich_repr(chart: Chart) -> None:
+    console = Console(highlight=False, force_terminal=True, width=WIDTH, height=HEIGHT)
+    with console.capture() as capture:
+        console.print(chart)
+
+    output = capture.get()
+    assert chart.info_bar.name in output
+
+
+def test_set_label(chart: Chart) -> None:
+    value = "foo"
+
+    for label in vars(constants.LABELS):
+        assert getattr(chart.info_bar.labels, label) != value
+        chart.set_label(label, value)
+        assert getattr(chart.info_bar.labels, label) == value
+
+
+def test_set_highlight(chart: Chart) -> None:
+    assert not chart.highlights
+
+    # Add highlighted prices
+    chart.set_highlight("4.2", "red")
+    chart.set_highlight("4.2", "green")
+    chart.set_highlight("2.4569", "orange")
+    assert chart.highlights == {"2.4569": "orange", "4.2": "green"}
+
+    # Remove a price
+    chart.set_highlight("2.4569", "")
+    assert chart.highlights == {"4.2": "green"}
+
+
+def test_set_name(chart: Chart) -> None:
+    name = "Magic Chart"
+    assert chart.info_bar.name != name
+
+    chart.set_name(name)
+    assert chart.info_bar.name == name
+
+
+def test_set_vol_bear_color(chart: Chart) -> None:
+    color = (3, 2, 1)
+    assert chart.volume_pane.bearish_color != color
+
+    chart.set_vol_bear_color(*color)
+    assert chart.volume_pane.bearish_color == color
+
+
+def test_set_vol_bull_color(chart: Chart) -> None:
+    color = (1, 2, 3)
+    assert chart.volume_pane.bullish_color != color
+
+    chart.set_vol_bull_color(*color)
+    assert chart.volume_pane.bullish_color == color
+
+
+def test_set_volume_pane_enabled(chart: Chart) -> None:
+    assert chart.volume_pane.enabled
+
+    chart.set_volume_pane_enabled(False)
+    assert not chart.volume_pane.enabled
+
+
+def test_set_volume_pane_unicode_fill(chart: Chart) -> None:
+    value = "°"
+
+    assert chart.volume_pane.unicode_fill != value
+
+    chart.set_volume_pane_unicode_fill(value)
+    assert chart.volume_pane.unicode_fill == value
+
+
+def test_update_candles(chart: Chart) -> None:
+    assert len(chart.chart_data.main_candle_set.candles) == 1
+
+    chart.update_candles([CANDLE] * 4, reset=True)
+    assert len(chart.chart_data.main_candle_set.candles) == 4
+
+    candle = CANDLE
+    candle.volume = 0.00001
+    chart.update_candles([candle])
+    assert len(chart.chart_data.main_candle_set.candles) == 5
+
+
+def test_update_size(chart: Chart) -> None:
+    assert chart.chart_data.terminal_size == (WIDTH, HEIGHT)
+
+    # No change
+    chart.update_size(WIDTH, HEIGHT)
+    assert chart.chart_data.terminal_size == (WIDTH, HEIGHT)
+
+    # New size
+    chart.update_size(WIDTH * 2, HEIGHT * 2)
+    assert chart.chart_data.terminal_size == (WIDTH * 2, HEIGHT * 2)
+
+    # No change
+    chart.update_size(WIDTH * 2, HEIGHT * 2)
+    assert chart.chart_data.terminal_size == (WIDTH * 2, HEIGHT * 2)
