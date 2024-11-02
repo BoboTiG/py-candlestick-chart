6de189203690bf1cdc36f9f9e305bbc5f33504ab
Mickaël Schoentgen <contact@tiger-222.fr>
2024-11-02 12:51:49 +0100

    feat(tests): improve `Candle` coverage

--- src/tests/test_chart_data.py
+++ src/tests/test_chart_data.py
@@ -6,10 +6,27 @@ from candlestick_chart.candle import Candle
 from candlestick_chart.chart_data import ChartData
 from candlestick_chart.volume_pane import VolumePane
 
+CANDLE = Candle(open=1, high=2, low=0.5, close=1.2)
+
 
 @pytest.fixture(scope="module")
 def chart_data() -> ChartData:
-    return ChartData([Candle(open=1, high=1, low=1, close=1)], width=100, height=50)
+    return ChartData([CANDLE], width=100, height=50)
+
+
+def test_candle_eq() -> None:
+    candle1 = Candle(open=1, high=2, low=0.5, close=1.2)
+    assert candle1 == CANDLE
+
+    candle2 = Candle(open=1.1, high=2.1, low=0.51, close=1.21)
+    assert candle2 != CANDLE
+
+    with pytest.raises(NotImplementedError):
+        assert object() == CANDLE
+
+
+def test_candle_repr() -> None:
+    assert repr(CANDLE) == "Candle<open=1.0, low=0.5, high=2.0, close=1.2, volume=0.0, timestamp=0.0, type=bullish>"
 
 
 def test_compute_height_take_into_account_constant_change(chart_data: ChartData) -> None:
